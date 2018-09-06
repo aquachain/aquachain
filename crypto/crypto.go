08a762f6790189cd1280d789fe16b1628f4d6729
aerth <aerth@riseup.net>
2018-09-05 21:01:15 -0700

    crypto, params: temporarily make hf8 and hf9 super easy

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -76,24 +76,24 @@ func Argon2idA(data ...[]byte) []byte {
 	return argon2.IDKey(buf.Bytes(), nil, argonTime, 1, argonThreads, common.HashLength)
 }
 
-// Argon2id calculates and returns the Argon2id hash of the input data, using 256mb mem
+// Argon2id calculates and returns the Argon2id hash of the input data, using 16kb mem
 func Argon2idB(data ...[]byte) []byte {
 	//fmt.Printf(".")
 	buf := &bytes.Buffer{}
 	for i := range data {
 		buf.Write(data[i])
 	}
-	return argon2.IDKey(buf.Bytes(), nil, argonTime, 1024*256, argonThreads, common.HashLength)
+	return argon2.IDKey(buf.Bytes(), nil, argonTime, 16, argonThreads, common.HashLength)
 }
 
-// Argon2id calculates and returns the Argon2id hash of the input data, using 512mb mem
+// Argon2id calculates and returns the Argon2id hash of the input data, using 32kb
 func Argon2idC(data ...[]byte) []byte {
 	//fmt.Printf(".")
 	buf := &bytes.Buffer{}
 	for i := range data {
 		buf.Write(data[i])
 	}
-	return argon2.IDKey(buf.Bytes(), nil, argonTime, 1024*512, argonThreads, common.HashLength)
+	return argon2.IDKey(buf.Bytes(), nil, argonTime, 32, argonThreads, common.HashLength)
 }
 
 // Argon2id calculates and returns the Argon2id hash of the input data.
