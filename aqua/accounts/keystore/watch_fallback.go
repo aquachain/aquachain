d1d831eef760c1dd16bae04e6f6cc2da3ad8f287
aerth <aerth@riseup.net>
2019-07-30 17:37:38 -0400

    remove keystore watcher for all platforms (bloat, failed on osx)

--- aqua/accounts/keystore/watch_fallback.go
+++ aqua/accounts/keystore/watch_fallback.go
@@ -14,11 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the aquachain library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build ios linux,arm64 windows !darwin,!freebsd,!linux,!netbsd,!solaris
-
-// This is the fallback implementation of directory watching.
-// It is used on unsupported platforms.
-
+// This is the default implementation of directory watching.
 package keystore
 
 type watcher struct{ running bool }
