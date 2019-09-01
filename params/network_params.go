cc5483bc097c0dfbaa7846cbeb84080358ebe1e3
aerth <aerth@riseup.net>
2019-09-01 19:43:15 -0400

    params: add ImmutabilityThreshold (from geth)

--- params/network_params.go
+++ params/network_params.go
@@ -23,4 +23,28 @@ const (
 	// BloomBitsBlocks is the number of blocks a single bloom bit section vector
 	// contains.
 	BloomBitsBlocks uint64 = 4096
+	// BloomBitsBlocksClient is the number of blocks a single bloom bit section vector
+	// contains on the light client side
+	BloomBitsBlocksClient uint64 = 32768
+
+	// BloomConfirms is the number of confirmation blocks before a bloom section is
+	// considered probably final and its rotated bits are calculated.
+	BloomConfirms = 256
+
+	// BloomTrieFrequency is the block frequency for creating BloomTrie on both
+	// server/client sides.
+	BloomTrieFrequency = 32768
+
+	// HelperTrieConfirmations is the number of confirmations before a client is expected
+	// to have the given HelperTrie available.
+	HelperTrieConfirmations = 2048
+
+	// HelperTrieProcessConfirmations is the number of confirmations before a HelperTrie
+	// is generated
+	HelperTrieProcessConfirmations = 256
+
+	// ImmutabilityThreshold is the number of blocks after which a chain segment is
+	// considered immutable (i.e. soft finality). It is used by the downloader as a
+	// hard limit against deep ancestors, by the blockchain against deep reorgs.
+	ImmutabilityThreshold uint64 = 900
 )
