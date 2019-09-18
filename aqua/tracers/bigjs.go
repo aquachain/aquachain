492c006ccbf1837c4cb34b44b4b0f1cd24f2116e
aerth <aerth@riseup.net>
2019-09-17 21:49:32 -0400

    aqua/tracers: save space for nocgo binary

--- aqua/tracers/bigjs.go
+++ aqua/tracers/bigjs.go
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the aquachain library. If not, see <http://www.gnu.org/licenses/>.
 
+// +build cgo
+
 package tracers
 
 // bigIntegerJS is the minified version of https://github.com/peterolson/BigInteger.js.
