eee683516ddf3d998e1b5ed5f338865c74e12dd4
aerth <aerth@riseup.net>
2019-09-21 19:19:59 -0400

    release: v1.7.10

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 1       // Major version component of the current release
-	VersionMinor = 7       // Minor version component of the current release
-	VersionPatch = 10      // Patch version component of the current release
-	VersionMeta  = "devel" // Version metadata to append to the version string
+	VersionMajor = 1  // Major version component of the current release
+	VersionMinor = 7  // Minor version component of the current release
+	VersionPatch = 10 // Patch version component of the current release
+	VersionMeta  = "" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
