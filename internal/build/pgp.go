eb9069281d6d0c43b776757a839a3c188bc80edf
aerth <aerth@riseup.net>
2020-03-31 20:45:49 -0400

    internal/build: remove pgp deps, unused

--- internal/build/pgp.go
+++ internal/build/pgp.go
@@ -32,28 +32,28 @@ import (
 //
 // Note, this method assumes a single key will be container in the pgpkey arg,
 // furthermore that it is in armored format.
-func PGPSignFile(input string, output string, pgpkey string) error {
-	// Parse the keyring and make sure we only have a single private key in it
-	keys, err := openpgp.ReadArmoredKeyRing(bytes.NewBufferString(pgpkey))
-	if err != nil {
-		return err
-	}
-	if len(keys) != 1 {
-		return fmt.Errorf("key count mismatch: have %d, want %d", len(keys), 1)
-	}
-	// Create the input and output streams for signing
-	in, err := os.Open(input)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
+// func PGPSignFile(input string, output string, pgpkey string) error {
+// 	// Parse the keyring and make sure we only have a single private key in it
+// 	keys, err := openpgp.ReadArmoredKeyRing(bytes.NewBufferString(pgpkey))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if len(keys) != 1 {
+// 		return fmt.Errorf("key count mismatch: have %d, want %d", len(keys), 1)
+// 	}
+// 	// Create the input and output streams for signing
+// 	in, err := os.Open(input)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer in.Close()
 
-	out, err := os.Create(output)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
+// 	out, err := os.Create(output)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer out.Close()
 
-	// Generate the signature and return
-	return openpgp.ArmoredDetachSign(out, keys[0], in, nil)
-}
+// 	// Generate the signature and return
+// 	return openpgp.ArmoredDetachSign(out, keys[0], in, nil)
+// }
