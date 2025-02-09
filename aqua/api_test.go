e1daefa37abb3d51e310a57531f200884d409cfa
aerth <aerth@localhost>
2025-02-09 23:54:39 +0000

    nocgo tracers

--- aqua/api_test.go
+++ aqua/api_test.go
@@ -14,9 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the aquachain library. If not, see <http://www.gnu.org/licenses/>.
 
-//go:build gccgo || cgo || !nocgo
-// +build gccgo cgo !nocgo
-
 package aqua
 
 import (
