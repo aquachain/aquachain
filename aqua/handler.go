d257a3268eecd3e01909dc4b4c30540cebb80b03
aerth <aerth@localhost>
2025-02-24 06:15:11 +0000

    aqua/handler: fix check nil

--- aqua/handler.go
+++ aqua/handler.go
@@ -196,7 +196,7 @@ func (pm *ProtocolManager) removePeer(id string) {
 		}
 	}
 	// Hard disconnect at the networking layer
-	if peer != nil {
+	if peer.Peer != nil {
 		peer.Peer.Disconnect(p2p.DiscUselessPeer)
 	}
 }
