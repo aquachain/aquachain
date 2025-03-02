11b45b3d607b27a747cbbfa06d6deeb3954eb00d
aerth <aerth@localhost>
2025-03-02 04:47:07 +0000

    cool

--- common/sense/env_test.go
+++ common/sense/env_test.go
@@ -6,6 +6,8 @@ func TestEnv1(t *testing.T) {
 	var nokeysmode = FeatureEnabled("NO_KEYS", "nokeys")
 	if nokeysmode {
 		println("test nokeys mode was found")
+	} else {
+		println("test nokeys mode was not found")
 	}
 
 }
