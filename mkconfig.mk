84e58066e94364fd041776f4d76eda6a16a01171
aerth <aerth@localhost>
2025-03-07 23:59:59 +0000

    ci: try uploading multiple artifacts
    version info is invalid on the autobuild artifacts (unlike tagged release build)
    (eg: just the commit, no v1.2.3) so we make a fake release version v0.0.0-{commit}

--- mkconfig.mk
+++ mkconfig.mk
@@ -14,19 +14,13 @@ GOPATH != ${GOCMD} env GOPATH
 # rebuild target if *any* go file changes
 GOFILES ?= $(shell find . -name '*.go' -type f -not \( -path "./vendor/*" -o -path "./build/*" \))
 COMMITHASH ?= $(shell git rev-parse --short=6 HEAD)
-version ?= $(shell git describe --tags --always --dirty)
-# patches_sinces ?= $(shell git rev-list ..HEAD --count)
-# is_dirty ?= $(shell git diff --quiet || echo "-dirty")
-# ifeq (-dirty,$(is_dirty))
-# version := $(version)-dirty
-# endif
-# #$(info GOCMD = $(GOCMD))
-# #$(info GOOS = $(GOOS))
-# #$(info GOARCH = $(GOARCH))
-# #$(info GOPATH = $(GOPATH))
-# #$(info version = $(version))
-# #$(info COMMITHASH = $(COMMITHASH))
 
+
+version ?= $(shell git describe --tags --always --dirty)
+# ci: fix version if not tagged ('checkout' actions module?)
+ifeq (,$(findstring v,$(version)))
+version := $(shell echo v0.0.0-$(version) | cut -c2-)
+endif
 
 # build flags and tags (TODO: use a separator instead of base64)
 tags ?= netgo osusergo static
